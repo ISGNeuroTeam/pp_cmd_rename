0abf66979a18dab365becfaeb4fec0d292784308
egor-lukyanov <elukianov@isgneuro.com>
2022-02-18 21:52:04 +0300

    bug fixed: value

--- rename/command.py
+++ rename/command.py
@@ -2,13 +2,13 @@ from pp_exec_env.base_command import BaseCommand, Syntax, Rule, pd
 
 
 class RenameCommand(BaseCommand):
-    syntax = Syntax([Rule(name="col", type="arg", input_types=['string', 'term'], inf=True)],
+    syntax = Syntax([Rule(name="col", type="arg", input_types=['string', 'term'], inf=True, )],  # must_be_a_field=True
                     use_timewindow=False)
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         cols = self.get_arg('col')
         df.rename(
-            {initial_name: initial_name.named_as for initial_name in cols}
+            {initial_name.value: initial_name.named_as for initial_name in cols}
         )
 
         return df
